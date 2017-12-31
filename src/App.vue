8b5187fbdcbe701ab84cbdd2e8d10275c631fd90
wacky6.AriesMBP <416707889@qq.com>
2017-12-31 13:19:47 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import "./typo.css"
+import { mapGetters } from 'vuex'
+import { checkToken } from 'store'
+
+export default {
+  name: 'app',
+  computed: {
+    token: 'token'
+  },
+  mounted() {
+    if (!checkToken(this.token)) {
+      this.$toast({
+        message: this.token ? '登录已过期，请重新登录' : '请先登录'
+      })
+      this.$router.push('/login/')
+    } else {
+      this.$router.push('/events/')
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import superagent from 'superagent'
+import superagentUse from 'superagent-use'
+import Icon from 'vue-awesome/components/Icon'
+import store from './store'
+import {
+  Button,
+  Cell,
+  Header,
+  Loadmore,
+  Toast
+} from 'mint-ui'
+
+Vue.config.productionTip = false
+Vue.component('Icon', Icon)
+Vue.component( Button.name, Button )
+Vue.component( Cell.name, Cell )
+Vue.component( Header.name, Header )
+Vue.component( Loadmore.name, Loadmore )
+Vue.prototype.$toast = Toast
+
+// superagent token injection
+Vue.prototype.$agent = superagentUse(superagent)
+Vue.prototype.$agent.use(req => {
+  const token = store.getters.token
+  token && req.auth(token, null, {type: 'bearer'})
+})
+Vue.prototype.$agent.use(req => {
+  req.body = () => new Promise((resolve, reject) =>
+    req.then(
+      res => resolve(res.body),
+      reject
+    )
+  )
+  req.blob = () => req.responseType('blob').body()
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
